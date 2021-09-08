512dc8435c8b4a8acbb9fba0630461a8eb3a9801
S1LV3R <s1lv3r@corax.team>
2021-09-08 16:41:46 +0200

    feat: create an actual footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,40 +1,76 @@
 <template>
   <footer>
-    <h2>Footer</h2>
+    <Logo id="logo" />
+    <div id="links">
+      <a
+        v-for="link in links"
+        :key="link.url"
+        :href="link.url"
+        target="_blank"
+        rel="noopener noreferrer"
+        ><fa-icon :icon="link.icon"
+      /></a>
+    </div>
+    <span></span>
   </footer>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+import { faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons'
+import Logo from './logo.vue'
+
+export default Vue.extend({
+  components: { Logo },
+  data() {
+    return {
+      links: [
+        { icon: faGithub, url: 'https://github.com/kodekafe' },
+        { icon: faDiscord, url: '/discord' },
+      ],
+    }
+  },
+})
 </script>
 
 <style lang="scss" scoped>
 :root {
-  &.dark-mode {
-    footer {
-      color: $darkBgColor;
-    }
+  &.dark-mode footer {
+    color: $darkBgColor;
   }
 
-  &.light-mode {
+  &.light-mode footer {
     color: $lightBgColor;
   }
 }
 
 footer {
-  margin: 0;
-  flex-shrink: 1;
-
-  & > a {
-    text-decoration: none;
-
-    .octicon {
-      vertical-align: baseline;
-    }
-  }
-
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
   background-color: $themeColor;
+  padding: 0 10%;
+}
+
+#logo {
+  display: inline-block;
+}
+
+#links {
+  display: inline-block;
+  margin: 0 auto;
+
+  & > * {
+    margin: 0.25rem;
+    color: currentColor;
+  }
+}
+
+span {
+  width: 1px;
+  height: 100%;
 }
 </style>
