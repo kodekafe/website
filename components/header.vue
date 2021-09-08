7f6df73dc64cd0351966abcdc2fa11320c1f0e63
S1LV3R <s1lv3r@corax.team>
2021-09-08 15:21:18 +0200

    refactor: move icon and logo text to own file

--- components/header.vue
+++ components/header.vue
@@ -1,11 +1,6 @@
 <template>
   <header>
-    <div>
-      <div id="logo-svg" class="icon">
-        <Logo />
-      </div>
-      <h2>KODEKAFÉ</h2>
-    </div>
+    <Logo />
     <nav>
       <NuxtLink to="/" exact>Home</NuxtLink>
       <a href="/discord" exact>Discord server</a>
@@ -28,8 +23,8 @@
 <script lang="ts">
 import Vue from 'vue'
 
-import Logo from '@/assets/icons/logo.svg?inline'
 import Octicon from '@/components/octicon.vue'
+import Logo from '@/components/logo.vue'
 
 export default Vue.extend({
   components: { Logo, Octicon },
@@ -109,52 +104,13 @@ header {
   white-space: nowrap;
 }
 
-#logo-svg {
-  max-width: min-content;
-  height: max-content;
-  display: inline-block;
-  vertical-align: middle;
-  & > svg {
-    vertical-align: middle;
-  }
-}
-
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  display: inline-block;
-  margin: 1.2rem 0;
-  padding: 0;
-  vertical-align: middle;
-}
-
 :root {
-  &.light-mode {
-    header {
-      color: $lightBgColor;
-    }
-    .icon {
-      color: $lightIconColor;
-
-      path {
-        fill: $lightIconColor;
-      }
-    }
+  &.light-mode header {
+    color: $lightBgColor;
   }
 
-  &.dark-mode {
-    header {
-      color: $darkBgColor;
-    }
-    .icon {
-      color: $darkIconColor;
-      path {
-        fill: $darkIconColor;
-      }
-    }
+  &.dark-mode header {
+    color: $darkBgColor;
   }
 }
 </style>
---
+++ components/logo.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div id="logo-svg" class="icon">
+      <Logo />
+    </div>
+    <h2>KODEKAFÉ</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import Logo from '@/assets/icons/logo.svg?inline'
+
+export default Vue.extend({
+  components: { Logo },
+})
+</script>
+
+<style lang="scss" scoped>
+#logo-svg {
+  max-width: min-content;
+  height: max-content;
+  display: inline-block;
+  vertical-align: middle;
+  & > svg {
+    vertical-align: middle;
+  }
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  display: inline-block;
+  margin: 1.2rem 0;
+  padding: 0;
+  vertical-align: middle;
+}
+
+:root {
+  &.light-mode.icon {
+    color: $lightIconColor;
+
+    path {
+      fill: $lightIconColor;
+    }
+  }
+
+  &.dark-mode.icon {
+    color: $darkIconColor;
+    path {
+      fill: $darkIconColor;
+    }
+  }
+}
+</style>
